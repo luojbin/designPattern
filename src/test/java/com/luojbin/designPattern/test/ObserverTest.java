48857e0bde5bc491057f73f9fb6243f0bedc1fc6
luoyanfu <yanfu.luo@vstecs.com>
2018-03-09 09:48:21 +0800

    观察者模式-1 自定义观察者, push模式

--- src/test/java/com/luojbin/designPattern/test/ObserverTest.java
+++ src/test/java/com/luojbin/designPattern/test/ObserverTest.java
@@ -1,13 +1,18 @@
 package com.luojbin.designPattern.test;
 
 import com.luojbin.designPattern.observer.displayElement.CurrentConditionDisplay;
+import com.luojbin.designPattern.observer.displayElement.DisplayElement;
+import com.luojbin.designPattern.observer.displayElement.ForecastDisplay;
+import com.luojbin.designPattern.observer.displayElement.StatisticsDisplay;
 import com.luojbin.designPattern.observer.subject.WeatherData;
 
 public class ObserverTest {
     public static void main(String[] args){
         WeatherData weatherData = new WeatherData();
 
-        CurrentConditionDisplay conditionDisplay = new CurrentConditionDisplay(weatherData);
+        DisplayElement conditionDisplay = new CurrentConditionDisplay(weatherData);
+        DisplayElement forecastDisplay = new ForecastDisplay(weatherData);
+        DisplayElement statisticDisplay = new StatisticsDisplay(weatherData);
 
 
         weatherData.setMeasurements(80,60,30.3f);
